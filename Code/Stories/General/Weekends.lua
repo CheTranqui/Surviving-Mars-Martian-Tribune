e8c1de0d17f12e8395f2b1903c18c2d81862c70e
CheTranqui <CheTranqui@users.noreply.github.com>
2018-08-30 13:22:00 -0500

    Update Weekends.lua

--- Code/Stories/General/Weekends.lua
+++ Code/Stories/General/Weekends.lua
@@ -11,7 +11,7 @@ local function CheckStory()
 		AddStory({
 			key = Key1,
 			title = T{9013855, "Children Struggling to Understand Concept of Weekends"},
-			story = T{9013856, "     Teachers on Mars have reported that Martianborn children struggle to comprehend the concept of a weekend. A weekend is an Earth Tradition, where for two sols (called 'days' on Earth) many citizens will cease working, and have a day \"off\". \"But then how does the colony survive?\" ask many of the children. Next, we will be trying to explain the concept of megacities to them."}
+			story = T{9013856, "     Teachers on Mars have reported that Martianborn children struggle to comprehend the concept of a weekend. A weekend is an Earth Tradition, where for two sols (called 'days' on Earth) many citizens will cease working and relax for a time instead. \"But how does the colony survive then?\" ask the children. Next week's cultural lesson revolves around capitals and life in a metropolis."}
 		})
 	end
 end
