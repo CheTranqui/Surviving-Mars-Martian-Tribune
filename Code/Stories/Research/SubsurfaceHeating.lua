ca269e20e9aefd8aa01172ad5ad30d7ffceaa2ce
Janeene Beeforth <gh@dawnmist.net>
2018-09-01 09:20:24 +1000

    Update Subsurface Heaters text.

--- Code/Stories/Research/SubsurfaceHeating.lua
+++ Code/Stories/Research/SubsurfaceHeating.lua
@@ -12,14 +12,14 @@ local function CheckStory()
 		if random_num == 1 then
 			AddStory({
 				key = Key1,
-				title = T{9013822, "Underground Radiators"},
-				story = T{9013823, "     The newest discovery in surviving the cold has become synonymous with the Martian underground scene, as the new \"subsurface heaters\" become quickly idolised and called saviours of the underground. A strange move, but with the ability to heat up an area the size of a small dome, the underground radiators could indeed save your life."}
+				title = T{9013822, "Local Warming: It's a Thing"},
+				story = T{9013823, "     The newest discovery in surviving the cold has become synonymous with the Martian underground scene, as the new \"subsurface heaters\" become quickly idolised and declared our molten saviours by many. Global warming of Mars might still be elusive, but at least we can now count on some artificial local warming."}
 			})
 		else
 			AddStory({
 				key = Key2,
-				title = T{9013824, "Please do not use Subsurface Heaters to make Hot Tubs"},
-				story = T{9013825, "     The Martian Tribune would like to advise its readers that the newly discovered subsurface heaters are not to be used to make your own personal hot tub, no matter how awesome it would be. It is a serious waste of Martian resources."}
+				title = T{9013824, "The Subsurface Heaters Are Not Hot Tubs"},
+				story = T{9013825, "     The Martian Tribune would like to advise its readers that the newly discovered subsurface heaters are not to be used as your own personal hot tub, no matter how awesome it might be, it would be a serious waste of Martian resources."}
 			})
 		end
 	end
